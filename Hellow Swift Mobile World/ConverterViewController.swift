1e41636d90885132aaf7f56444ea333ea8890a9b
Ilya Pavlov <ipavlov92@gmail.com>
2016-04-07 16:56:54 +0300

    converter works

--- Hellow Swift Mobile World/ConverterViewController.swift
+++ Hellow Swift Mobile World/ConverterViewController.swift
@@ -2,34 +2,44 @@
 //  ConverterViewController.swift
 //  Hellow Swift Mobile World
 //
-//  Created by Victoria Fastovec on 06.04.16.
+//  Created by Victoria Fastovec on 07.04.16.
 //  Copyright © 2016 Ilya Pavlov. All rights reserved.
 //
 
 import UIKit
 
 class ConverterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    let rubToUsdMultiplier = 1 / 67.67
+    let usdToRubMultiplier = 63.0
+    
+    @IBOutlet weak var rubInput: UITextField!
+    @IBOutlet weak var usdInput: UITextField!
+    
+    @IBAction func rubToUsdPressed(sender: AnyObject) {
+        convertToUsd()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func usdToRubPressed(sender: AnyObject) {
+        convertUsdToRub()
+        
     }
-    */
+    
+    func convertToUsd(){
+        guard let rubs = Double(rubInput.text!)else {
+            return
+        }
+        let result = String(rubs * rubToUsdMultiplier)
+        usdInput.text = result
+    }
+    func convertUsdToRub(){
+        guard let usd = Double(usdInput.text!) else {
+            return
+        }
+        let result = String(usd * usdToRubMultiplier)
+        rubInput.text = result
+    
+    }
 
+  
 }
